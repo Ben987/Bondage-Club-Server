651aa6d0be467c9eda74cfdd7f9603bfbc315a0f
Ace1331 <>
2020-08-05 11:42:09 -0400

    safety net

--- app.js
+++ app.js
@@ -475,7 +475,7 @@ function ChatRoomSearch(data, socket) {
 			// Builds a list of all public rooms, the last rooms created are shown first
 			var CR = [];
 			var C = 0;
-			for (var C = ChatRoom.length - 1; C >= 0; C--)
+			for (var C = ChatRoom.length - 1; ((C >= 0) && (CR.length <= 60)); C--)
 				if (ChatRoom[C] != null)
 					if ((Acc.Environment == ChatRoom[C].Environment) && (Space == ChatRoom[C].Space)) // Must be in same environment (prod/dev) and same space (hall/asylum)
 						if (ChatRoom[C].Ban.indexOf(Acc.MemberNumber) < 0) // The player cannot be banned
