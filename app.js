56abb9274e55b02a01d2ea678f0418dfbc329fc1
Ben987 <30805159+Ben987@users.noreply.github.com>
2021-09-08 13:57:44 -0400

    Crash Fix
    Crash Fix

--- app.js
+++ app.js
@@ -488,28 +488,11 @@ function AccountUpdate(data, socket) {
 
 				// Some changes should be synched to other players in chatroom
 				const Acc = Account[P];
-				if (Acc.ChatRoom &&
-					[
-						"AssetFamily",
-						"Title",
-						"Reputation",
-						"Description",
-						"LabelColor",
-						"ItemPermission",
-						"Inventory",
-						"BlockItems",
-						"LimitedItems",
-						"FavoriteItems",
-						"OnlineSharedSettings",
-						"WhiteList",
-						"BlackList"
-					].some(k => data[k] != null)
-				) {
+				if ((Acc != null) && Acc.ChatRoom && ["AssetFamily", "Title", "Reputation", "Description", "LabelColor", "ItemPermission", "Inventory", "BlockItems", "LimitedItems", "FavoriteItems", "OnlineSharedSettings", "WhiteList", "BlackList"].some(k => data[k] != null))
 					ChatRoomSyncCharacter(Acc.ChatRoom, Acc.MemberNumber, Acc.MemberNumber);
-				}
 
 				// If we have data to push
-				if (!ObjectEmpty(data)) Database.collection("Accounts").updateOne({ AccountName : Account[P].AccountName }, { $set: data }, function(err, res) { if (err) throw err; });
+				if ((Acc != null) && !ObjectEmpty(data)) Database.collection("Accounts").updateOne({ AccountName : Acc.AccountName }, { $set: data }, function(err, res) { if (err) throw err; });
 				break;
 
 			}
